27b98eecefbaa52a88ce85a5e06d43daccb85bd5
Watcher <monojit0288@gmail.com>
2026-03-24 11:06:15 +0530

    minor issue fix v 0.4

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
   const token = useSelector((state) => state.auth.token);
